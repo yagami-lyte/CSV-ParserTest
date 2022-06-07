c22e3600e418b02bb5431f7623f11db1f15d5f39
shreyag@1 <shreyag@sahaj.ai>
2022-06-07 12:33:04 +0530

    Added get routes

--- src/main/kotlin/server/GreetingController.kt
+++ src/main/kotlin/server/GreetingController.kt
@@ -1,24 +1,35 @@
 package server
 
-import database.Connector
 import database.DatabaseOperations
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import routeHandler.getRouteHandler.getResponse.ConfigNames
-import routeHandler.getRouteHandler.getResponse.ErrorPage
 import routeHandler.getRouteHandler.getResponse.HomePage
-import routeHandler.postRouteHandler.postResponse.HandleCSVMetaData
-import routeHandler.postRouteHandler.postResponse.HandleCsv
-import routeHandler.postRouteHandler.postResponse.SendConfigurations
 
 
 @RestController
 class GreetingController {
 
+    private val homePage = HomePage()
+    private val configNames = ConfigNames(DatabaseOperations(database.Connector()))
+
     @GetMapping("/")
     fun getHTML() :String {
-        return "Hello Sahajeevs"
+        return homePage.getResponse("/index.html")
+    }
+
+    @GetMapping("/main.js")
+    fun getJs() :String {
+        return homePage.getResponse("/main.js")
+    }
+
+    @GetMapping("/main.css")
+    fun getCSS() :String {
+        return homePage.getResponse("/main.css")
+    }
+
+    @GetMapping("/get-config-files")
+    fun getConfigFiles() :String {
+        return  configNames.getResponse("/get-config-files")
     }
 }
